fe01c0306abb3412cffd47947c144bc49a8e0aca
Wesley Taylor <jghjgfgh@gmail.com>
2025-02-18 07:44:17 -0500

    Initial TWAP stuff.

--- db/0013-fixup.up.sql
+++ db/0013-fixup.up.sql
@@ -292,3 +292,177 @@ $$;
 -- select *
 -- from base
 -- where interv - interv_lag > interval '30 seconds';
+
+
+
+
+CREATE TABLE twaps_chart_data
+(
+  interv TIMESTAMPTZ NOT NULL,
+  token_amount NUMERIC NOT NULL,
+  market_acct TEXT NOT NULL,
+  bar_size INTERVAL NOT NULL,
+  PRIMARY KEY (market_acct, interv, bar_size)
+);
+
+
+CREATE FUNCTION generate_forward_filled_twaps()
+RETURNS VOID
+LANGUAGE PLPGSQL
+AS
+$$
+DECLARE
+  market_record RECORD;
+BEGIN
+    FOR market_record IN (
+        WITH proposal_pass_markets AS (
+            SELECT
+                CASE
+                    WHEN pass_market_acct IS NOT NULL THEN pass_market_acct
+                    ELSE NULL
+                END AS market_acct,
+                created_at,
+                COALESCE(ended_at, completed_at, NOW()) AS ended_at
+            FROM proposals
+        ),
+        proposal_fail_markets AS (
+            SELECT
+                CASE
+                    WHEN fail_market_acct IS NOT NULL THEN fail_market_acct
+                    ELSE NULL
+                END AS market_acct,
+                created_at,
+                COALESCE(ended_at, completed_at, NOW()) AS ended_at
+            FROM proposals
+        ),
+        metric_markets AS (
+            SELECT
+                amm_addr AS market_acct,
+                created_at,
+                COALESCE(completed_at, NOW()) AS ended_at
+            FROM v0_4_metric_decisions
+            WHERE amm_addr IS NOT NULL
+        ),
+        distinct_markets AS (
+            SELECT * FROM proposal_pass_markets
+            UNION ALL
+            SELECT * FROM proposal_fail_markets
+            UNION ALL
+            SELECT * FROM metric_markets
+        ),
+        spot_markets AS (
+            SELECT DISTINCT ON (market_acct)
+                market_acct,
+                daos.created_at AS created_at,
+                NOW() AS ended_at
+            FROM markets
+            LEFT JOIN daos ON daos.base_acct = markets.market_acct
+            WHERE market_acct NOT IN (SELECT market_acct FROM distinct_markets)
+              AND proposal_acct IS NULL
+              AND daos.base_acct IS NOT NULL
+            ORDER BY market_acct, created_at
+        ),
+        all_markets AS (
+            SELECT * FROM spot_markets
+            UNION ALL
+            SELECT * FROM distinct_markets
+        ),
+        pre_final AS (
+            SELECT market_acct,
+                   created_at,
+                   ended_at,
+                   last_update,
+                   COALESCE(last_update + INTERVAL '30 seconds', TIME_BUCKET(INTERVAL '30 seconds', created_at)) AS start_ts,
+                   TIME_BUCKET(INTERVAL '30 seconds', LEAST(NOW() - INTERVAL '30 seconds', ended_at)) AS end_ts
+            FROM all_markets
+            NATURAL LEFT JOIN
+            (
+              SELECT market_acct,
+                     MAX(interv) AS last_update
+              FROM twaps_chart_data
+              WHERE bar_size = INTERVAL '30 seconds'
+              GROUP BY market_acct
+            ) AS tt
+        )
+        SELECT *
+        FROM pre_final
+        WHERE start_ts <= end_ts
+          AND (
+              last_update IS NOT NULL
+              OR EXISTS (
+                   SELECT 1
+                   FROM twaps
+                   WHERE twaps.market_acct = pre_final.market_acct
+                     AND twaps.created_at >= pre_final.start_ts
+                     AND twaps.created_at <= pre_final.end_ts + INTERVAL '30 seconds'
+                   LIMIT 1
+              )
+          )
+    ) LOOP
+        INSERT INTO twaps_chart_data (
+            interv, token_amount, market_acct, bar_size
+        )
+        WITH series AS (
+            SELECT generate_series(
+                market_record.start_ts,
+                market_record.end_ts,
+                INTERVAL '30 seconds'
+            ) AS time_series_generated
+        ),
+        matching_twaps_data AS (
+            SELECT
+                TIME_BUCKET(INTERVAL '30 seconds', twaps.created_at) AS interv,
+                LAST(twaps.token_amount, twaps.created_at) AS token_amount,
+                twaps.market_acct
+            FROM twaps
+            JOIN markets ON markets.market_acct::text = twaps.market_acct::text
+              AND markets.market_acct = market_record.market_acct
+            WHERE twaps.created_at >= market_record.start_ts
+              AND twaps.created_at <= market_record.end_ts + INTERVAL '30 seconds'
+            GROUP BY TIME_BUCKET(INTERVAL '30 seconds', twaps.created_at), twaps.market_acct
+        ),
+        including_prev AS (
+            SELECT interv, token_amount, market_acct FROM matching_twaps_data
+            UNION ALL
+            (
+              SELECT interv, token_amount, market_acct
+              FROM twaps_chart_data
+              WHERE market_acct = market_record.market_acct
+                AND interv < market_record.start_ts
+                AND bar_size = INTERVAL '30 seconds'
+              ORDER BY interv DESC NULLS LAST
+              LIMIT 1
+            )
+        ),
+        including_next AS (
+            SELECT * FROM including_prev
+            UNION ALL
+            SELECT market_record.end_ts + INTERVAL '30 seconds' AS interv,
+                   NULL::NUMERIC AS token_amount,
+                   market_record.market_acct AS market_acct
+            WHERE market_record.end_ts NOT IN (SELECT interv FROM matching_twaps_data)
+        ),
+        with_lags AS (
+            SELECT market_acct,
+                   interv,
+                   lag(interv, 1) OVER (PARTITION BY market_acct ORDER BY interv) AS interv_lag,
+                   lag(token_amount, 1) OVER (PARTITION BY market_acct ORDER BY interv) AS token_amount_lag
+            FROM including_next
+        ),
+        ffill_bars AS (
+            SELECT generate_series(interv_lag + INTERVAL '30 seconds', interv - INTERVAL '30 seconds', INTERVAL '30 seconds') AS interv,
+                   token_amount_lag AS token_amount,
+                   market_acct
+            FROM with_lags
+            WHERE interv - interv_lag > INTERVAL '30 seconds'
+        ),
+        final_union AS (
+            SELECT * FROM matching_twaps_data
+            UNION ALL
+            SELECT * FROM ffill_bars
+        )
+        SELECT *, INTERVAL '30 seconds' AS bar_size FROM final_union
+        ON CONFLICT (market_acct, interv, bar_size) DO NOTHING;
+    END LOOP;
+END;
+$$;
